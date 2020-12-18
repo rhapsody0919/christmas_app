35eb15bd6e293b8865cf56e4260ccdf4b2640bc4
ryohei-next <bassii5757@gmail.com>
2020-12-18 16:19:20 +0900

    [update] 修正

--- task_message.php
+++ task_message.php
@@ -5,17 +5,8 @@ loginedSession();
 
 // テーブルに表示
 $dbh = dbConnect();
-$sql = 'SELECT con1_users.name, con1_users.class, con1_task_messages.title, con1_task_messages.message, con1_task_messages.id FROM con1_users INNER JOIN con1_task_messages ON con1_users.id = con1_task_messages.user_id';
-$data = $dbh->query($sql);
-
-//編集処理（絶賛途中）
-
-
-//削除処理（絶賛途中）
-$members = $dbh->prepare('SELECT * FROM con1_users WHERE id=?');
-$members->execute(array($_SESSION['id']));
-$members = $members->fetch();
-
+$sql = 'SELECT con1_users.name, con1_users.class, con1_task_messages.user_id, con1_task_messages.title, con1_task_messages.message, con1_task_messages.id FROM con1_users INNER JOIN con1_task_messages ON con1_users.id = con1_task_messages.user_id';
+$task_messages = $dbh->query($sql);
 
 ?>
 
@@ -34,13 +25,20 @@ $members = $members->fetch();
 <tr>
 <th>class</th><th>Name</th><th>Title</th><th>edit</th>
 </tr>
-<?php foreach ($data as $datum) : ?>
+<?php foreach ($task_messages as $task_message) : ?>
 <tr>
-<td><?php echo $datum['class']; ?></td>
-<td><?php echo $datum['name']; ?></td>
-<td><a href="task_message_detail.php?class=<?php echo $datum['class']; ?>&name=<?php echo $datum['name']; ?>&title=<?php echo $datum['title']; ?>&message=<?php echo $datum['message']; ?>">
-<?php echo $datum['title']; ?></a></td>
-<td><a href="edit_task_message.php?task_message_id=<?php echo $datum['id']; ?>">編集</a>／<a href="delete_task_message.php?task_message_id=<?php echo $datum['id']; ?>">削除</a></td>
+<td><?php echo $task_message['class']; ?></td>
+<td><?php echo $task_message['name']; ?></td>
+<td>
+<a href="task_message_detail.php?task_message_id=<?php echo $task_message['id']; ?>">
+<?php echo $task_message['title']; ?></a>
+</td>
+<td>
+<?php if ((int)$_SESSION['id'] === (int)$task_message['user_id']) : ?>
+<a href="edit_task_message.php?task_message_id=<?php echo $task_message['id']; ?>">編集</a>／
+<a href="delete_task_message.php?task_message_id=<?php echo $task_message['id']; ?>">削除</a>
+<?php endif; ?>
+</td>
 </tr>
 <?php endforeach; ?>
 </table>
